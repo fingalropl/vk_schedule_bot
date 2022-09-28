fa03fa33cf1293d7aad2b8f416e287128a952acc
Кирилл Чемизов <kirill20.04.2002@yandex.ru>
2022-09-28 22:30:27 +0300

    добавил "расписание на завтра. оптимизировал get_shedule"

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_but(text):
 
 def get_keyboard(text):
     return {
-        'one_time' : False,
+        'one_time' : True,
         'buttons' : [
             [get_but(text)]
         ]
@@ -51,7 +51,7 @@ def json_keyboard(text):
 def send_mes(id, text, img):
     print(img)
     if img is not True:
-        api.messages.send(chat_id = id, message = text, random_id = 0)
+        api.messages.send(chat_id = id, message = text, random_id = 0, )
 
 
 def send_doggy(id, text):
@@ -62,16 +62,18 @@ def send_video(id, text, vid):
     api.messages.send(chat_id = id, message = text, random_id = 0, attachment = vid)
 
 
-def send_shedule(id):
-    shedule_comment = get_shedule()
-    text = shedule_comment['text']
-    shedule = shedule_comment['img']
+#weekday - показатель дня недели; '1' - завтра, '0' - сегодня
+def send_shedule(id, weekday):
+    shedule, text = get_shedule(weekday)
     api.messages.send(chat_id = id, message = text, random_id = 0, attachment = shedule)
 
 
 def send_hi(id, text):
     api.messages.send(chat_id = id, message = text, random_id = 0)
 
+
+def off_button(id, but_text):
+    api.messages.send(chat_id = id, message = 'Нажмите, на кнопку "Вырубить кнопку", чтобы отключить старую кнопку.', keyboard = json_keyboard(but_text), random_id = 0)
 
 def main():
     for event in longpoll.listen():
@@ -83,9 +85,14 @@ def main():
             elif msg == 'ты всего лишь робот, твоя жизнь не имеет смысла!':
                 send_video(id=id, text=' ', vid='video341601157_456239127')
             elif msg == 'расписание':
-                send_shedule(id=id)
+                send_shedule(id=id, weekday=0)
             elif msg == 'привет':
                 send_hi(id=id, text = 'Привет. Можешь попросить расписание командой "расписание", фотку собачки - "Собачку!" еще можешь написать (лучше не надо) - "ты всего лишь робот, твоя жизнь не имеет смысла!". Отмечать меня в этих сообщениях не надо. Предложения по улучшениям пиши моему создателю "vk.com/fingalropl".')
+            elif msg == 'расписание на завтра':
+                send_shedule(id=id, weekday=1)
+            elif msg == 'выключить кнопку':
+                off_button(id=id, but_text = 'Вырубить кнопку.')
+
 
 if __name__ == '__main__':
     main()
